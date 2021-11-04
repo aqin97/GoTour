34034cb0c0d7d05cf000a06c31c1b3b6f9ab4d7a
aqin97 <791041423@qq.com>
2021-11-04 23:27:25 +0800

    bug修复

--- internal/sql2struct/template.go
+++ internal/sql2struct/template.go
@@ -7,16 +7,13 @@ import (
 	"text/template"
 )
 
-const structTpl = `type {{ .Tablename | ToCamelCase }} struct {
-{{ range .Columns }} {{ $length := len .Comment }} {{ if gt $length 0 }}//
-{{ .Comment }} {{ else }}// {{ .Name }} {{ end }}
-	{{ $typeLen := len .Type }} {{ if gt $typeLen 0 }} {{ .Name | ToCamelCase }}
-	{{ .Type }}	{{.Tag}} {{ else }} {{.Name}}{{ end }}
-{{ end }}
+const structTpl = `type {{ .TableName | ToCamelCase }} struct {
+{{ range .Columns }} {{ $length := len .Comment }} {{ if gt $length 0 }}//{{.Comment }} {{ else }}// {{ .Name }} {{ end }}
+{{ $typeLen := len .Type }} {{ if gt $typeLen 0 }} {{ .Name | ToCamelCase }}	{{ .Type }}	{{.Tag}} {{ else }} {{.Name}}{{ end }}{{ end }}
+}
 
 func (model {{ .TableName | ToCamelCase }}) TableName() string {
 	return "{{ .TableName }}"
-}
 }`
 
 type StructTemplate struct {
